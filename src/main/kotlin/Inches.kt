562b20ba8fa6c33b76e837cedc512d35a0a4779a
Lucas Bubner <lkbubner@proton.me>
2024-03-07 19:59:42 +1030

    Update Inches class

--- src/main/kotlin/Inches.kt
+++ src/main/kotlin/Inches.kt
@@ -1,6 +1,6 @@
 /**
  * Utility to convert between inches and metric units.
- * [src](https://github.com/Murray-Bridge-Bunyips/BunyipsFTC/blob/stable/TeamCode/BunyipsLib/src/main/java/org/murraybridgebunyips/bunyipslib/Inches.java)
+ * [src](https://github.com/Murray-Bridge-Bunyips/BunyipsLib/blob/b0662cde8c7231a8e5ea4edcf72b1cf4042afe6a/src/main/java/org/murraybridgebunyips/bunyipslib/Inches.java)
  */
 object Inches {
     @JvmStatic
@@ -19,6 +19,26 @@ object Inches {
     }
 
     @JvmStatic
+    fun toFieldTiles(inches: Double): Double {
+        return inches / 23
+    }
+
+    @JvmStatic
+    fun toHammerUnits(inches: Double): Double {
+        return inches / 0.75
+    }
+
+    @JvmStatic
+    fun toFootballFields(inches: Double): Double {
+        return inches * 3600.0
+    }
+
+    @JvmStatic
+    fun toLightYears(inches: Double): Double {
+        return inches / 3.725e+17
+    }
+
+    @JvmStatic
     fun fromCM(cm: Double): Double {
         return cm / 2.54
     }
@@ -31,5 +51,25 @@ object Inches {
     @JvmStatic
     fun fromM(m: Double): Double {
         return m / 0.0254
+    }
+
+    @JvmStatic
+    fun fromFieldTiles(tiles: Double): Double {
+        return tiles * 23
+    }
+
+    @JvmStatic
+    fun fromHammerUnits(units: Double): Double {
+        return units * 0.75
+    }
+
+    @JvmStatic
+    fun fromFootballFields(ff: Double): Double {
+        return ff / 3600.0
+    }
+
+    @JvmStatic
+    fun fromLightYears(ly: Double): Double {
+        return ly * 3.725e+17
     }
 }
